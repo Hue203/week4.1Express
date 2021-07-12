e87c03f3da798f0a60c438c358b76621a330eefe
Hue203 <huenguyenmt.vn@gmail.com>
2021-07-12 16:11:36 +0700

    foos

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require("express");
-const logger = require("morgan");
 require("dotenv").config();
+const logger = require("morgan");
+const cors = require("cors");
 const app = express();
 const port = 5000;
-const cors = require("cors");
-
-app.use(cors());
-app.get("/", (req, res) => {
-  res.send("HelloThreee!");
-});
+app.use(express.json());
 app.use(logger("dev"));
+app.use(cors());
+const { foos } = require("./foo.json");
+///Create//
+
+app.post("/foos", (req, res) => {
+  foos.push({ ...req.body });
+  res.send("Create item in foo");
+});
+///Get//
+
+app.get("/foos", (req, res) => {
+  // res.send("Hello Threee!");
+  res.send(foos);
+});
+///Update//
+
+app.patch("/foos/:id", (req, res) => {
+  let idx = foos.findIndex((f) => {
+    return f.id === parseInt(req.params.id);
+  });
+  const foo = foos[idx];
+
+  foo[idx] = {
+    ...foo,
+    ...req.body,
+  };
+
+  res.send(foos);
+});
+
+///Delete//
+app.delete("/foos/:id", (req, res) => {
+  foos = foos.filter((f) => {
+    return f.id !== parseInt(req.params.id);
+  });
+  res.send(foos);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
